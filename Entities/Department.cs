7afc401a74b24ef5f9aa8b86b838c070f6a4f15d
Ícaro Monteiro <icarosilvamonteiro@gmail.com>
2022-08-31 22:35:28 -0300

    Commit inicial do estudo - Criação das classes e alguns métodos

---
+++ Entities/Department.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ex24___enum.Entities
+{
+    internal class Department
+    {
+        public string Name { get; set; }
+
+        public Department()
+        {
+        }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ Entities/Worker.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using ex24___enum.Entities.Enums;
+
+namespace ex24___enum.Entities
+{
+    internal class Worker
+    {
+        public string Name { get; set; }
+        public WorkerLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }//referenciando o objeto Department
+        public List<HourContract> Contracts { get; set; } = new List<HourContract>();//referenciando o objeto HourContract
+
+        public Worker()
+        {
+        }
+
+        public Worker(string name, WorkerLevel level, double baseSalary, Department department)
+        {
+            Name = name;
+            Level = level;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+
+        public void AddContract(HourContract contract)
+        {
+            Contracts.Add(contract);
+        }
+
+        public void RemoveContract(HourContract contract)
+        {
+            Contracts.Remove(contract);
+        }
+
+        public double Income(int year, int month)
+        {
+            double sum = BaseSalary;
+
+            foreach (HourContract contract in Contracts)
+            {
+                if (contract.Date.Year == year && contract.Date.Month == month)
+                {
+                    sum += contract.TotalValue();
+                }
+            }
+        }
+    }
+}
